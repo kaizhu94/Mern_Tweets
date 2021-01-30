f5ff718b69e713c2d99678ab60ef1e7389b64543
kaizhu94 <zhukai0803@gmail.com>
2021-01-30 15:50:42 -0500

    mongo

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI
 
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log("Connected to MongoDB successfully"))
+    .catch(err => console.log(err));
+
+// app.get("/", (req, res) => res.send("Hello Kai"));
 app.get('/', (req, res)=>{
-    res.send("Hi, I am Kai");
+    console.log(res)
+    res.send("Hi, I am not Kai");
 })
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, ()=>{
-    console.log(`I am listening on port ${port}`)
-})
+app.listen(port, () => console.log(`Server is running on port ${port}`));

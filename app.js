68017cf3d100808b0cb14b5fc098907472de7241
kaizhu94 <zhukai0803@gmail.com>
2021-01-30 18:43:41 -0500

    Auth

--- app.js
+++ app.js
@@ -2,17 +2,39 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const db = require("./config/keys").mongoURI
+const users = require("./routes/api/users")
+const tweets = require("./routes/api/tweets")
+const bodyParser = require('body-parser');
+const User = require('./models/User')
 
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
 
-// app.get("/", (req, res) => res.send("Hello Kai"));
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+
+app.use(bodyParser.json());
+
 app.get('/', (req, res)=>{
-    debugger;
-    res.send("Hi, I am not Kai");
+    const user = new User({
+        handle: "kai",
+        email: 'kai@gmail.com',
+        password: 'kai123456'
+    });
+    user.save();
+    res.send("Hi, I am Kai");
 })
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
+
 
 const port = process.env.PORT || 5000;
 

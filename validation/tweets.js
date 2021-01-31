8df0a9ed3892e862a72eaabb97270240da902a9a
kaizhu94 <zhukai0803@gmail.com>
2021-01-31 17:46:36 -0500

    start frontend

--- validation/tweets.js
+++ validation/tweets.js
@@ -4,15 +4,14 @@ const validText = require('./valid-text');
 module.exports = function validateTweetInput(data) {
   let errors = {};
   
-
   data.text = validText(data.text) ? data.text : '';
 
-  if (!Validator.isLength(data.text, { min: 5, max: 140 })) {
-    errors.text = 'Tweet must be between 5 and 140 characters';
+  if (!Validator.isLength(data.text, { min: 5, max: 160 })) {
+    errors.text = 'Tweet must be between 5 and 160 characters';
   }
 
   if (Validator.isEmpty(data.text)) {
-    errors.text = 'Text field is required';
+    errors.text = 'Text cannot be blank';
   }
 
   return {
